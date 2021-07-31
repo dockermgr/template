a6aed2e3d1544b1ae6f6e81d816ba3ac825c385f
Jason <jason@casjay.net>
2021-07-31 12:01:36 -0400

    🦈🏠🐜❗ Initial Commit ❗🐜🦈🏠

---
+++ README.md
@@ -0,0 +1,10 @@
+# 👋 Welcome to dockermgr templates 👋
+
+### to install
+```shell
+dockermgr install template
+```  
+## Author  
+
+👤 **Jason Hempstead**  
+
---
+++ install.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+APPNAME="$(basename "$0")"
+VERSION="202107311147-git"
+USER="${SUDO_USER:-${USER}}"
+HOME="${USER_HOME:-${HOME}}"
+SRC_DIR="${BASH_SOURCE%/*}"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#set opts
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+##@Version       : 202107311147-git
+# @Author        : casjay
+# @Contact       : casjay
+# @License       : WTFPL
+# @ReadME        : dockermgr --help
+# @Copyright     : Copyright: (c) 2021 casjay, casjay
+# @Created       : Saturday, Jul 31, 2021 11:47 EDT
+# @File          : template
+# @Description   : Template for dockermgr
+# @TODO          : 
+# @Other         : 
+# @Resource      : 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Import functions
+CASJAYSDEVDIR="${CASJAYSDEVDIR:-/usr/local/share/CasjaysDev/scripts}"
+SCRIPTSFUNCTDIR="${CASJAYSDEVDIR:-/usr/local/share/CasjaysDev/scripts}/functions"
+SCRIPTSFUNCTFILE="${SCRIPTSAPPFUNCTFILE:-testing.bash}"
+SCRIPTSFUNCTURL="${SCRIPTSAPPFUNCTURL:-https://github.com/dfmgr/installer/raw/$GIT_DEFAULT_BRANCH/functions}"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+if [ -f "$PWD/$SCRIPTSFUNCTFILE" ]; then
+  . "$PWD/$SCRIPTSFUNCTFILE"
+elif [ -f "$SCRIPTSFUNCTDIR/$SCRIPTSFUNCTFILE" ]; then
+  . "$SCRIPTSFUNCTDIR/$SCRIPTSFUNCTFILE"
+else
+  echo "Can not load the functions file: $SCRIPTSFUNCTDIR/$SCRIPTSFUNCTFILE" 1>&2
+  exit 1
+fi
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# user system devenv dfmgr dockermgr fontmgr iconmgr pkmgr systemmgr thememgr wallpapermgr
+system_install
+__options "$@"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Begin installer 
+APPNAME="template"
+DATADIR="${DOCKERMGR_HOME:-/srv/docker/$APPNAME}"
+DOCKER_HUB_URL="template"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+sudo mkdir -p "$DATADIR"/{data}
+sudo chmod -Rf 777 "$DATADIR"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+if docker ps -a | grep "$APPNAME" >/dev/null 2>&1; then
+  sudo docker pull "$DOCKER_HUB_URL"
+  sudo docker restart "$APPNAME"
+else
+  sudo docker run -d \
+    --name="$APPNAME" \
+    --hostname "$APPNAME" \
+    --restart=always \
+    --privileged \
+    -p 4040:80 \
+    -v "$DATADIR/data":/airsonic/data:z \
+    "$DOCKER_HUB_URL"
+fi
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# End script
+exit $?
